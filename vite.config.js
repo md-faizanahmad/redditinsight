5bad7dca9b4a075c24eb22085fd76a8c0e5aa936
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-04-10 22:37:16 +0530

    force clean deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:5173",
+      "/api": "https://reddit-insight-xi.vercel.app/",
     },
   },
 });
