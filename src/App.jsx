182a3dacb6caec7b13b0ab1851cfbf15f8685495
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-04-10 09:42:35 +0530

    feat: src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,16 @@
-import { useState } from "react";
+import Header from "./components/layouts/Header";
+import PostList from "./components/PostList";
+import SearchBar from "./components/SearchBar";
 import { useRedditSearch } from "./hooks/useRedditSearch";
 
 export default function App() {
-  const [query, setQuery] = useState("");
   const { posts, loading, error, search } = useRedditSearch();
 
   return (
-    <div>
-      <input
-        type="text"
-        placeholder="Search Reddit..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-
-      <button onClick={() => search(query)}>Search</button>
-
-      {loading && <p>Loading...</p>}
-      {error && <p>{error}</p>}
-
-      {posts.map((post) => (
-        <div key={post.id}>
-          <h3>{post.title}</h3>
-          <p>
-            👍 {post.upvotes} | r/{post.subreddit}
-          </p>
-        </div>
-      ))}
+    <div className="p-4">
+      <Header />
+      <SearchBar onSearch={search} />
+      <PostList posts={posts} loading={loading} error={error} />
     </div>
   );
 }
