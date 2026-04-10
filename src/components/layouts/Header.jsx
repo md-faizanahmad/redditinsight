d263ff6ba18b0ae8ededf42abe468025241c37e0
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-04-10 09:40:20 +0530

    feat: src/components/layouts/Header.jsx

---
+++ src/components/layouts/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header className="text-center py-10">
+      <h1 className="text-4xl font-bold">Reddit Search</h1>
+      <p className="text-gray-500 mt-2">
+        Find insights from Reddit discussions
+      </p>
+    </header>
+  );
+}
